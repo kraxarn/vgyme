9c11c0935f2bc3fc5ee1549d477b1c63f9f6cc88
kraxarn <emil202821@hotmail.com>
2018-08-04 17:56:30 +0200

    Remove some unused stuff

--- app/src/main/java/com/crow/vgyme/MainActivity.java
+++ app/src/main/java/com/crow/vgyme/MainActivity.java
@@ -1,8 +1,5 @@
 package com.crow.vgyme;
 
-import android.app.FragmentManager;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,11 +7,7 @@ import com.crow.vgyme.fragment.FragmentSettings;
 
 public class MainActivity extends AppCompatActivity
 {
-	private FragmentManager fragmentManager;
-
 	private FragmentSettings fragmentSettings;
-
-	private SharedPreferences prefs;
 
 	public void setTitle(String title)
 	{
@@ -28,12 +21,6 @@ public class MainActivity extends AppCompatActivity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-
-		// Create preferences
-		prefs = PreferenceManager.getDefaultSharedPreferences(this);
-
-		// Create fragment manager
-		fragmentManager = getFragmentManager();
 
 		// Create fragment
 		fragmentSettings = new FragmentSettings();
